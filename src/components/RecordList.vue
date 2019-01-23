19198fdfb3770e8e0928ed60729226bbfcc9fb27
Arattian <poghosyanmisak97@gmail.com>
2019-01-23 15:05:08 +0400

    Selected delete icon styled.

--- src/components/RecordList.vue
+++ src/components/RecordList.vue
@@ -6,6 +6,7 @@
       v-if="keys.hashKey"
       size="mini"
       @row-dblclick="showEditModal"
+      tooltip-effect="light"
       @selection-change="handleRowSelection"
       )
       el-table-column(type="selection" width="50")
@@ -21,7 +22,8 @@
       el-table-column(v-for="(header, index) of header" :show-overflow-tooltip="true" :prop="header" :label="header" title="Something" :key="index" v-if="hideHashKey(header)")
       el-table-column(fixed="right" width="50")
         template(slot="header" slot-scope="slot")
-          i(class="el-icon-delete delete" v-if="selectedItems" @click="showGroupDeleteModal" title="Delete Selected")
+          .delete-selected
+            i(class="el-icon-delete" v-if="selectedItems" @click="showGroupDeleteModal" title="Delete Selected")
         template(slot-scope="scope")
           span(class="delete-column")
             i(class="el-icon-delete delete" @click="showDeleteModal(scope.row)" title="Delete Raw")
@@ -57,6 +59,22 @@
   .delete:hover
     color #ff6d6d
     cursor pointer
+
+  .delete-selected
+    display flex
+    justify-content center
+    align-items center
+
+  .delete-selected i
+    color #ff6d6d
+    cursor pointer
+
+  .delete-selected i:hover
+    color #ff4747
+    cursor pointer
+
+  .delete-selected i
+    font-size 1.8em
 
   .el-table
     width 100%
